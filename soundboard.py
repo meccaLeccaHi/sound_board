fa63a1614369ae5531e573b9d9834516150624d8
meccaLeccaHi <ajones173@gmail.com>
2020-05-06 12:49:43 -0700

    finalized sounds

--- soundboard.py
+++ soundboard.py
@@ -9,30 +9,43 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
 GPIO.setup(24, GPIO.IN)
 GPIO.setup(25, GPIO.IN)
+GPIO.setup(26, GPIO.IN)
+GPIO.setup(27, GPIO.IN)
 
 pygame.mixer.init(48000, -16, 1, 1024)
 
-sndA = pygame.mixer.Sound(sound_dir+"CastleThunder.wav")
-sndB = pygame.mixer.Sound(sound_dir+"applause.wav")
-sndC = pygame.mixer.Sound(sound_dir+"WilhelmScream.wav")
+applause = pygame.mixer.Sound(sound_dir+"applause.wav")
+yay = pygame.mixer.Sound(sound_dir+"yay.wav")
+boo = pygame.mixer.Sound(sound_dir+"boo.wav")
+laugh = pygame.mixer.Sound(sound_dir+"laugh.wav")
+cricket = pygame.mixer.Sound(sound_dir+"cricket.wav")
+# buzzer = pygame.mixer.Sound(sound_dir+"buzzer.wav")
 
 soundChannelA = pygame.mixer.Channel(1)
 soundChannelB = pygame.mixer.Channel(2)
 soundChannelC = pygame.mixer.Channel(3)
+soundChannelD = pygame.mixer.Channel(4)
+soundChannelE = pygame.mixer.Channel(5)
 
 print("Sampler Ready.")
 
 while True:
-   try:
-      if (GPIO.input(23) == True):
-         soundChannelA.play(sndA)
-         print('sndA')
-      if (GPIO.input(24) == True):
-         soundChannelB.play(sndB)
-         print('sndB')
-      if (GPIO.input(25) == True):
-         soundChannelC.play(sndC)
-         print('sndC')
-      sleep(.1)
-   except KeyboardInterrupt:
-      exit()
+    try:
+        if (GPIO.input(23) == True):
+            soundChannelA.play(laugh)
+            print('chan 23')
+        if (GPIO.input(24) == True):
+            soundChannelB.play(applause)
+            print('chan 24')
+        if (GPIO.input(25) == True):
+            soundChannelC.play(boo)
+            print('chan 25')
+        if (GPIO.input(26) == True):
+            soundChannelD.play(cricket)
+            print('chan 26')
+        if (GPIO.input(27) == True):
+            soundChannelE.play(yay)
+            print('chan 27')
+        sleep(.1)
+    except KeyboardInterrupt:
+        exit()
