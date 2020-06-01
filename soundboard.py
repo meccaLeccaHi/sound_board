41cb20b7decc58fe6329f72590f7a040d54c65cd
meccaLeccaHi <ajones173@gmail.com>
2020-06-01 10:17:57 -0700

    updated script

--- soundboard.py
+++ soundboard.py
@@ -15,12 +15,14 @@ GPIO.setup(27, GPIO.IN)
 pygame.mixer.init(48000, -16, 1, 1024)
 
 # applause = pygame.mixer.Sound(sound_dir+"applause.wav")
-# yay = pygame.mixer.Sound(sound_dir+"yay.wav")
+audience_laugh = pygame.mixer.Sound(sound_dir+"audience_laugh.wav")
+yay = pygame.mixer.Sound(sound_dir+"yay.wav")
 boo = pygame.mixer.Sound(sound_dir+"boo.wav")
+boo.set_volume(.7)
 laugh = pygame.mixer.Sound(sound_dir+"laugh.wav")
-cricket = pygame.mixer.Sound(sound_dir+"cricket.wav")
+laugh.set_volume(.7)
 ahh = pygame.mixer.Sound(sound_dir+"ahh.wav")
-gasp = pygame.mixer.Sound(sound_dir+"gasp.wav")
+ahh.set_volume(.7)
 # buzzer = pygame.mixer.Sound(sound_dir+"buzzer.wav")
 
 soundChannelA = pygame.mixer.Channel(1)
@@ -34,7 +36,7 @@ print("Sampler Ready.")
 while True:
     try:
         if (GPIO.input(23) == True):
-            soundChannelA.play(gasp)
+            soundChannelA.play(audience_laugh)
             print('chan 23')
         if (GPIO.input(24) == True):
             soundChannelB.play(ahh)
@@ -43,7 +45,7 @@ while True:
             soundChannelC.play(boo)
             print('chan 25')
         if (GPIO.input(26) == True):
-            soundChannelD.play(cricket)
+            soundChannelD.play(yay)
             print('chan 26')
         if (GPIO.input(27) == True):
             soundChannelE.play(laugh)
